8af7e2404183b4cf5a3ac68f3a1dcfa869ea374d
Gregor <60974575+KramerGr@users.noreply.github.com>
2022-08-27 10:14:05 +0200

    Create the FeedItem model

---
+++ Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Gregor Kramer on 27.08.22.
+//
+
+import Foundation
+  
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
