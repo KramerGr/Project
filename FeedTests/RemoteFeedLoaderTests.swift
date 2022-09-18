614f0f63484a1621cf07fbec0365d53ba6782c02
Gregor <60974575+KramerGr@users.noreply.github.com>
2022-09-18 12:32:25 +0200

    Remove space

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -41,7 +41,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
-        
         let clientError = NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
 
