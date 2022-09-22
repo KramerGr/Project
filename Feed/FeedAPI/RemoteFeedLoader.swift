3cd03a7a08dc8cc36e77c2a127bda8f8b60b5dab
Gregor <60974575+KramerGr@users.noreply.github.com>
2022-09-22 07:30:38 +0200

    Remove space

--- Feed/FeedAPI/RemoteFeedLoader.swift
+++ Feed/FeedAPI/RemoteFeedLoader.swift
@@ -31,7 +31,6 @@ public final class RemoteFeedLoader {
                 completion(.invalidData)
             } else {
                 completion(.connectivity)
-                
             }
         }
     } 
